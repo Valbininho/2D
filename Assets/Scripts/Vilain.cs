18a2dce52f8d0dca476c3fc5edac0e898b0281df
Valentin <valentinbf94@gmail.com>
2021-10-04 17:58:30 +0200

    Code +

---
+++ Assets/Scripts/Vilain.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller;
+{
+    if boxCollider2D 
+}
+
+public class Vilain : MonoBehaviour
+{
+    void Start()
+    {
+        PlayerKiller float = false
+    }
+
+    void Update()
+    {
+        if
+    }
+}

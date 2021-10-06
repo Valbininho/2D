2dba92384095a13cac1fb29c25019acb569c9dbd
Valentin <valentinbf94@gmail.com>
2021-10-06 17:00:35 +0200

    Reprise vilain (timer)

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    private bool vilain2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vilain2 = GetComponent<Vilain>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

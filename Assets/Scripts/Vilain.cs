5a868d8e2ee99c4f313b70668fcd4a12941d9942
Valentin <valentinbf94@gmail.com>
2021-10-06 12:25:40 +0200

    Code bien avancé

--- Assets/Scripts/Vilain.cs
+++ Assets/Scripts/Vilain.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerKiller;
-{
-    if boxCollider2D 
-}
 
 public class Vilain : MonoBehaviour
 {
+
+    public float speed;
+    private Transform target;
+
     void Start()
     {
-        PlayerKiller float = false
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update()
     {
-        if
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name);
+    }
+
 }
